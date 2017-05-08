11fa63ab3e743ea37b75f3ab0294047408b15e25
Marcelo Alaniz <alanizmarcelo@gmail.com>
2017-05-08 19:08:16 -0300

    daniel and me changes

--- front/app/scripts/controllers/main.js
+++ front/app/scripts/controllers/main.js
@@ -29,14 +29,46 @@ app.controller('MainCtrl', function ($scope, $http) {
   $scope.url = 'http://leagueathletics.com';
   $scope.isLoading = false;
   $scope.getData = function(url) {
-
     $scope.isLoading = true;
     $http
-      .get('http://localhost:8080/rec/?url=' + url)
+      .get('https://gist.githubusercontent.com/DanielSchaffer/1311d617bb5dd27bd9f6b891a2d06298/raw/22d8072d5d32212509184ebc88160edfcabb3131/results.json')
       .then(function(res) {
         $scope.isLoading = false;
         $scope.data = res.data    
       })
   }
 
+  $scope.search = function(url) {
+    console.log(url);
+
+  }
+});
+
+function recSearch(items, s) {
+  
+  var ritems = items.map( function (x) {
+    if (x.url.search(s) !== -1) {
+      return x;
+    } else if (x.childs){
+      return {
+        url: x.url,
+        childs: recSearch( x.childs, s)
+      };
+    }
+  })
+  ritems = ritems.filter(function(x) {
+    console.log(x);
+    return x!== undefined;
+  })
+  return ritems;
+}
+
+app.filter('recfilter', function() {
+  return function (input, x) {
+    if (!x || !input) { 
+      return input;
+    } else {
+      return recSearch(input, x);
+    }
+  }
 });
