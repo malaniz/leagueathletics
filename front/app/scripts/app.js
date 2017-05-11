859014231024ff5346f26dd09dbd1b04b89f1f8b
Marcelo Alaniz <alanizmarcelo@gmail.com>
2017-05-11 19:53:11 -0300

    adding tree funcionality and filter

--- front/app/scripts/app.js
+++ front/app/scripts/app.js
@@ -1,35 +1,23 @@
 'use strict';
 
-/**
- * @ngdoc overview
- * @name crawlerApp
- * @description
- * # crawlerApp
- *
- * Main module of the application.
- */
-angular
-  .module('crawlerApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+var app = angular.module('crawlerApp', [
+  'ngAnimate',
+  'ngCookies',
+  'ngResource',
+  'ngRoute',
+  'ngSanitize',
+  'ngTouch',
+  'ui.tree'
+]);
+
+app.config(function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/main.html',
+      controller: 'MainCtrl',
+      controllerAs: 'main'
+    })
+  .otherwise({
+    redirectTo: '/'
   });
+});
--- front/app/scripts/controllers/main.js
+++ front/app/scripts/controllers/main.js
@@ -1,74 +1,87 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name crawlerApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the crawlerApp
- */
 var app = angular.module('crawlerApp')
-/*
-app.controller('MainCtrl', function ($scope, $http) {
-  $scope.tree = [{referer: 'http://leagueathletics.com', nodes: [], isLoading: false}];
-  $scope.getData = function(data) {
-    data.isLoading = true;
-    $http
-      .get('http://localhost:8080/getsite/?url=' + data.referer)
-      .then(function(res) {
-        data.isLoading = false;
-        data.nodes = res.data.map(function(x) {
-          return { referer: x, nodes: [], isLoading: false}
-        });
-      })
-  }
-});
-*/
 
-app.controller('MainCtrl', function ($scope, $http) {
-  $scope.url = 'http://leagueathletics.com';
-  $scope.isLoading = false;
-  $scope.getData = function(url) {
-    $scope.isLoading = true;
-    $http
-      .get('https://gist.githubusercontent.com/DanielSchaffer/1311d617bb5dd27bd9f6b891a2d06298/raw/22d8072d5d32212509184ebc88160edfcabb3131/results.json')
-      .then(function(res) {
-        $scope.isLoading = false;
-        $scope.data = res.data    
-      })
-  }
-
-  $scope.search = function(url) {
-    console.log(url);
-
-  }
-});
-
-function recSearch(items, s) {
-  
-  var ritems = items.map( function (x) {
-    if (x.url.search(s) !== -1) {
-      return x;
-    } else if (x.childs){
+function deepSearch(data, s) {
+  var regexp = new RegExp(s, 'gi');
+  var mapped = [];
+  if (data.childs) {
+    mapped = data.childs.map(function(x){
+      if (regexp.test(x.url)) {
+        return { url: x.url }
+      } else {
+        return deepSearch(x, s)
+      }
+    }).filter(function(x) {
+      return x !== false;
+    })
+    if (mapped.length > 0) {
       return {
-        url: x.url,
-        childs: recSearch( x.childs, s)
-      };
+        url: data.url,
+        childs: mapped      
+      }
+    } else {
+      return false;
     }
-  })
-  ritems = ritems.filter(function(x) {
-    console.log(x);
-    return x!== undefined;
-  })
-  return ritems;
+  } else {
+    if (regexp.test(data.url)) {
+      return { url: data.url } 
+    } else {
+      return false
+    }
+  }
 }
 
-app.filter('recfilter', function() {
-  return function (input, x) {
-    if (!x || !input) { 
-      return input;
-    } else {
-      return recSearch(input, x);
-    }
+app.controller('MainCtrl', function ($scope, $rootScope, $timeout, crawler) {
+  $scope.url = 'http://leagueathletics.com';
+  $scope.step = 1;
+  $scope.getData = function(url) {
+    crawler
+      .get(url)
+      .then(function(data){
+        $scope.data = data;
+        $scope.step = 2;
+      })
+  }
+
+  $scope.togg = function(scope) {
+    scope.toggle();
+  }
+  $scope.goBack = function() {
+    $scope.step = 1;
+  }
+  $scope.deepfilter = function(data, s) {
+    $scope.data = ( (!s) || (s === "")) ? 
+      crawler.links() : 
+      deepSearch(crawler.links(), s);
   }
 });
+
+
+app.factory('crawler', function($http, $q, $rootScope) {
+  var gurl = 'https://gist.githubusercontent.com/DanielSchaffer/1311d617bb5dd27bd9f6b891a2d06298/raw/22d8072d5d32212509184ebc88160edfcabb3131/results.json';
+  var links = {};
+  return {
+    links: function() {
+      return angular.copy(links);
+    },
+    get: function(url) {
+      console.log(url);
+      return $q(function(resolve, reject) {
+        $rootScope.isLoading = true;
+        $http
+          .get(gurl)
+          .then(function(res) {
+            $rootScope.isLoading = false;
+            links = angular.copy(res.data);
+            resolve(res.data)   
+          }, function(err) {
+            $rootScope.isLoading = false;
+            reject(err);
+          })
+      })
+    }
+  }
+})
+
+
